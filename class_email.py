a4701a4b9837de24e94ffe9f25e0eb033893ec06
Haftamu Desta <haftamudesta>
2025-11-12 10:42:50 -0800

    Add Inbox class

--- class_email.py
+++ class_email.py
@@ -8,10 +8,24 @@ class Email:
 
     def mark_as_read(self):
         self.read = True
+
+    def display_full_email(self):
+        self.mark_as_read()
+        print('\n--- Email ---')
+
+
 class User:
     def __init__(self, name):
         self.name = name
-        self.inbox = []
+        self.inbox = Inbox()
 
     def send_email(self, receiver, subject, body):
         email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+        receiver.inbox.receive_email(email)
+
+class Inbox:
+    def __init__(self):
+        self.emails = []
+    
+    def receive_email(self, email):
+        self.emails.append(email)
